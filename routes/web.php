7969ce48f7ad87536d878ce1e94285f84184ffd4
naswrrr <nasywa24si@mahasiswa.pcr.ac.id>
2025-10-18 17:41:01 +0700

    Memperbaiki template dan menyederhanakan template Project

--- routes/web.php
+++ routes/web.php
@@ -2,15 +2,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
-Route::get('/login', [AuthController::class, 'index'])->name('auth.index');
-Route::post('/login', [AuthController::class, 'login'])->name('auth.login');
-Route::get('/register', [AuthController::class, 'indexRegister'])->name('auth.register');
-Route::post('/register', [AuthController::class, 'register'])->name('auth.register'); //
-
-Route::get('/admin/dashboard', function () {
-    return view('dashboard');
-})->name('admin.dashboard');
-
 Route::get('/', function () {
     return view('guest');
 });
