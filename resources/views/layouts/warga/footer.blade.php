1e36416d49e3a9eaa1af7a257b1b0bb90ff954e3
naswrrr <nasywa24si@mahasiswa.pcr.ac.id>
2025-10-29 20:55:59 +0700

    before uts

---
+++ resources/views/layouts/warga/footer.blade.php
@@ -0,0 +1,25 @@
+    {{-- start footer --}}
+    <!-- Copyright Start -->
+    <div class="container-fluid copyright py-4">
+        <div class="container">
+            <div class="row g-4 align-items-center">
+                <div class="col-md-4 text-center text-md-start mb-md-0">
+                    <span class="text-body"><a href="#"><i class="fas fa-copyright text-light me-2"></i>Portal
+                            Bina Desa</a>, All right
+                        reserved.</span>
+                </div>
+                <div class="col-md-4 text-center">
+                    <div class="d-flex align-items-center justify-content-center">
+                        <a href="#" class="btn-hover-color btn-square text-white me-2"><i
+                                class="fab fa-facebook-f"></i></a>
+                        <a href="#" class="btn-hover-color btn-square text-white me-2"><i
+                                class="fab fa-twitter"></i></a>
+                        <a href="#" class="btn-hover-color btn-square text-white me-2"><i
+                                class="fab fa-instagram"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Copyright End -->
+    {{-- end footer --}}
