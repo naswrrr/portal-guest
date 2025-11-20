16b62f8c087292ec60d046d9c503e28b5a3fff4b
nasywa <nasywa24si@mahasiswa.pcr.ac.id>
2025-11-20 22:47:49 +0700

    P11

--- database/seeders/WargaSeeder.php
+++ database/seeders/WargaSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
-use Faker\Factory;
+use Faker\Factory as Faker;
 
 class WargaSeeder extends Seeder
 {
